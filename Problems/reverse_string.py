ea7c715c7508c13144d65849295c928998dfdb77
ankitaingle <aningle31@gmail.com>
2024-03-25 16:36:35 -0400

    Function to reverse a string using slice

---
+++ Problems/reverse_string.py
@@ -0,0 +1,19 @@
+def reverse_string(s):
+    reversed_string = ""
+    i = len(s) - 1
+    while True:
+        reversed_string += s[i]
+        i = i - 1
+        if i < 0:
+            break
+    return reversed_string
+
+
+def reverse_string_v2(s):
+    """This method utilizes Python's slicing notation. In Python, string[start:stop:step] allows you to extract a
+    portion of a string based on the specified step size. By specifying -1 as the step, it tells Python to start from
+    the end of the string and move towards the beginning, effectively reversing the string."""
+    return s[::-1]
+
+
+print(reverse_string_v2("abcd"))
